b712838772a47fa6a3866cf7f2734eeb34cbf922
Alexey Karaev <99138558+avkarcr@users.noreply.github.com>
2023-01-03 21:58:06 +0300

    Update and rename 2_massa_create_service.sh to 3_massa_create_service.sh
    added restart service & filename

--- 3_massa_create_service.sh
+++ 3_massa_create_service.sh
@@ -2,7 +2,6 @@ echo
 read -p "Type your Massa password: " massa_password
 echo "export massa_password=$massa_password" >> ~/.profile
 source ~/.profile
-
 printf "[Unit]
 Description=Massa Node
 After=network-online.target
@@ -15,3 +14,6 @@ RestartSec=3
 LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/massad.service
+systemctl daemon-reload
+systemctl enable massad
+systemctl restart massad
