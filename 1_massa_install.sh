e812ddde53e078d49764c71ea1cab5a3f01482ef
Alexey Karaev <99138558+avkarcr@users.noreply.github.com>
2023-01-03 21:11:40 +0300

    Update 1_massa_install.sh
    Removed pwd

--- 1_massa_install.sh
+++ 1_massa_install.sh
@@ -4,14 +4,14 @@ echo "There is a good practice to install the node under the Screen."
 echo "To do that type \"screen -S install\" BEFORE running this script."
 echo "You can always hide the current screen by typing \"ctrl+a d\" (during install process)."
 echo
-echo "To continue type a PASSWORD for your Massa node (save your password manually) and press enter OR type type \"ctrl+c\" to exit."
+echo "To continue type ENTER or type type \"ctrl+c\" to exit."
 echo "==============================================================================================================================="
 echo
-read -p "Type your Massa password: " massa_password
-echo "export massa_password=\"$massa_password\"" >> ~/.profile
-source ~/.profile
+read -p "Type ENTER to continue..." dummy
+# echo "export massa_password=\"$massa_password\"" >> ~/.profile
+# source ~/.profile
 apt update && apt upgrade -y
-apt install -y curl htop pkg-config git build-essential libssl-dev libclang-dev-y
+apt install -y curl htop pkg-config git build-essential libssl-dev libclang-dev
 massa_version=$(curl -s "https://api.github.com/repos/massalabs/massa/releases/latest" | grep '"tag_name":' | sed  's/.*\(TEST..*\)\".*/\1/')
 wget -qO massa.tar.gz https://github.com/massalabs/massa/releases/download/$massa_version/massa_${massa_version}_release_linux.tar.gz &&  tar zxvf massa.tar.gz
 sudo tee <<EOF >/dev/null $HOME/massa/massa-node/config/config.toml
@@ -20,7 +20,4 @@ routable_ip = "$(curl ifconfig.me)"
 EOF
 ln -s ~/massa/massa-node/massa-node /usr/bin/massa-node
 #ln -s ~/massa/massa-node/massa-client /usr/bin/massa-client
-echo
-echo "PWD is: $massa_password"
-sleep 20
-massa-node -p $massa_password
+massa-node
